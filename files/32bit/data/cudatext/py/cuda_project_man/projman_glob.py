1591cee51704bdaa2365ea9a9d67d034529f22a8
Begasus <begasus@gmail.com>
2023-06-15 09:05:25 +0200

    hpkg files

--- files/32bit/data/cudatext/py/cuda_project_man/projman_glob.py
+++
@@ -1,4 +0,0 @@
-MASKS_IGNORE = '.rar .exe .dll .git .svn'
-MASKS_ZIP = '.zip .7z .tar .gz .tgz .rar .xz .cab .deb .rpm'
-MASKS_IMAGES = '.png .jpg .jpeg .gif .bmp .ico .webp .psd .tga .cur'
-MASKS_BINARY = '.exe .dll .o .a .msi .lib .obj .pdf .so'
